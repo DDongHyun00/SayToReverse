bf3d6c13510e6383b10044814d4234d0689b62d1
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-06-08 16:31:08 +0900

    init: first commit with backend structure and gitignore applied

---
+++ backend/src/main/java/org/example/saytoreverse/SayToReverseApplication.java
@@ -0,0 +1,13 @@
+package org.example.saytoreverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayToReverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SayToReverseApplication.class, args);
+    }
+
+}
