9c7620cd41efdde2eef7e12b6485555a84e2f3b6
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-06-10 17:47:22 +0900

    feat : addFilterBefore 코드 추가

--- backend/src/main/java/org/example/saytoreverse/config/SecurityConfig.java
+++ backend/src/main/java/org/example/saytoreverse/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package org.example.saytoreverse.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.saytoreverse.config.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -16,8 +21,10 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // REST API에선 CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // 회원가입/로그인 허용
-                        .anyRequest().authenticated()               // 그 외는 인증 필요
-                );
+                        .anyRequest().authenticated())               // 그 외는 인증 필요
+                // .addFilterBefore(내필터, 기준이되는기존필터.class) - 기준 필터 전에 내 필터를 먼저 실행, Username~Filter는 스프링 시큐리티자체에서 로그인에 사용하는 기본필터
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
