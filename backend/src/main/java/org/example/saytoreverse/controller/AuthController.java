614e513717adf0c4d42a90ddc416dc742d8f88ac
DDongHyun00 <117076978+DDongHyun00@users.noreply.github.com>
2025-06-11 11:29:24 +0900

    feat : logout & reissue 기능 구현

--- backend/src/main/java/org/example/saytoreverse/controller/AuthController.java
+++ backend/src/main/java/org/example/saytoreverse/controller/AuthController.java
@@ -1,5 +1,6 @@
 package org.example.saytoreverse.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.example.saytoreverse.dto.LoginRequestDto;
@@ -31,5 +32,19 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
         authService.login(requestDto, response);
         return ResponseEntity.ok("로그인 성공! 토큰 발급 완료");
+    }
+    // 로그아웃
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response){
+        authService.logout(request,response);
+        return ResponseEntity.ok("로그아웃 완료");
+    }
+
+
+    // 토큰 재발급
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response){
+        authService.reissue(request,response);
+        return ResponseEntity.ok("AccessToken 재발급 완료");
     }
 }
